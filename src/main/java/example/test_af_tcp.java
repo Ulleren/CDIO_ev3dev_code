a7803371f6dbc754c248ca716f31145b86203c10
Ulleren <ulrikbp@gmail.com>
2023-03-01 15:07:49 +0100

    test_af_tcp lavet, så der er kan snakkes over tcp med en server

--- src/main/java/example/test_af_tcp.java
+++ src/main/java/example/test_af_tcp.java
@@ -5,11 +5,42 @@ import ev3dev.actuators.lego.motors.EV3MediumRegulatedMotor;
 import ev3dev.sensors.Battery;
 import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-public class MyFirstRobot {
+public class test_af_tcp {
 
-    public static void main(final String[] args){
+    public static void main(final String[] args) throws IOException {
 
+        System.out.println("Foer");
+        //givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect();
+        GreetClient client = new GreetClient();
+        client.startConnection("192.168.29.191", 6666);
+        System.out.println("Efter start conn");
+        final EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.A);
+        final EV3LargeRegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.B);
+        final EV3MediumRegulatedMotor hatch = new EV3MediumRegulatedMotor(MotorPort.C);
+        final int motorSpeed = 100 * (int) Battery.getInstance().getVoltage();
+        motorLeft.setSpeed(motorSpeed);
+        motorRight.setSpeed(motorSpeed);
+        hatch.setSpeed(180);
+        String response;
+        do {
+            response = client.sendMessage("Got it");
+            if (response.equals("GO")) {
+                motorLeft.forward();
+                motorRight.forward();
+                Delay.msDelay(500);
+                motorLeft.stop();
+                motorRight.stop();
+
+            }
+            System.out.println(response);
+        } while (!response.equals("exit"));
+        /*
         System.out.println("Creating Motor A & B & C");
         final EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.A);
         final EV3LargeRegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.B);
@@ -24,16 +55,16 @@ public class MyFirstRobot {
             }
         }));
 
+
         System.out.println("Defining the Stop mode");
         motorLeft.brake();
         motorRight.brake();
 
         System.out.println("Defining motor speed");
-        final int motorSpeed = 100*(int)Battery.getInstance().getVoltage();
+        final int motorSpeed = 100 * (int) Battery.getInstance().getVoltage();
         motorLeft.setSpeed(motorSpeed);
         motorRight.setSpeed(motorSpeed);
         hatch.setSpeed(180);
-
 
 
         System.out.println("Go Forward with the motors");
@@ -67,7 +98,8 @@ public class MyFirstRobot {
 
         System.out.println("Checking Battery");
         System.out.println("Votage: " + Battery.getInstance().getVoltage());
-
+*/
         System.exit(0);
     }
 }
+
